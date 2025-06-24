6f572a506597dc9044f7f132754cf1f9b4917096
Mateus Barbosa Gromowski <mateusbarbosagromowski@gmail.com>
2025-06-24 15:23:41 -0300

    Delete main.java

--- main.java
+++
@@ -1,9 +0,0 @@
-public class main{
-	public static void main(String[] args) {
-		for (int a = 0; a <= 100; ++a) {
-			if ((a % 3 == 0) || (a % 5 ==0)) {
-				System.out.printf(a+ " ");
-			}
-		}
-	}
-}
